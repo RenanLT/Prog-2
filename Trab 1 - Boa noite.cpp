74eee324dbedce3f5225993031a2f9cf7c59edb5
RenanLT <36648451+RenanLT@users.noreply.github.com>
2018-02-20 19:20:15 -0300

    Add files via upload

---
+++ Trab 1 - Boa noite.cpp
@@ -0,0 +1,21 @@
+//Faça uma função em C que exiba a frase " Boa noite " na tela. Faça um programa em C para chamar a função.
+// Link para enviar: http:/bit.ly/prog2t1
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+void saudacao()
+{
+		printf("Boa noite !\n\n");
+
+	
+}
+
+int main()
+{
+	saudacao();
+	
+system ("pause");
+return 0;
+}
